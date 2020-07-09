4e0057e998fb2e98ab6bddd4a6ebad97b2014596
Andy Adams <andrewdcadams@gmail.com>
2020-07-09 22:38:49 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -4,7 +4,7 @@ def my_find(collection)
   i = 0
   while i < collection.length
     
-    return collection[i] if yield(collection[i])
+    return collection[i] if yield(collection[i]) # if it gets yielded and is true, return it 
     i += 1 
   end
 
